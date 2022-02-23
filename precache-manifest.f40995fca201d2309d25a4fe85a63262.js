ce0019d6978c8dfd91f6ad065c56d761ae054e32
Suren <ya.nyamsuren@gmail.com>
2022-02-23 08:17:30 +0800

    Updates

---
+++ precache-manifest.f40995fca201d2309d25a4fe85a63262.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179c6cd2400101f6b25975c4f1f32384",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "05ede333c7d54b25ac85",
+    "url": "/burger/static/css/main.b19f72c7.chunk.css"
+  },
+  {
+    "revision": "edd2b4700924d82c7690",
+    "url": "/burger/static/js/2.b34ee93c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.b34ee93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ede333c7d54b25ac85",
+    "url": "/burger/static/js/main.21d309f5.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
